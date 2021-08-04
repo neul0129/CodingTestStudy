b191f5f058f7e8ddaa9de2f54d62df34f807529a
neul0129 <neul0129@naver.com>
2021-08-04 15:04:40 +0900

    main

--- 3-2/3-2_sw.js
+++ 3-2/3-2_sw.js
@@ -1,30 +1,16 @@
 //큰 수의 법칙
+let sum = 0;  //합계
+const M = 8;  // 더할때 숫자 갯수
+const K = 3;  // 연속할 수 있는 최고의 수 갯수
 
-const count = 0;
-const sum = 0;
-const N = 5;
-const M = 8;
-const K = 3;
+const arr = [2,4,5,4,6]; 
 
-const arr = [2,4,5,4,6];
-
-arr.sort(); // [2,4,4,5,6]
+arr.sort(); // 낮은수부터 정렬 [2,4,4,5,6]
 const arr_top = arr[arr.length-1]; //배열중 가장 큰 값
 const arr_top2 = arr[arr.length-2]; //배열중 가장 큰 값
 
-    // for(arr.length; arr<M; arr.length++){ 
-    //     count++;
-    //     if(count % K != 0){
-    //         sum += arr_top;
-    //     }
-    //     else{
-    //         sum += arr_top2;
-    //     }
-    // }
-
-    for(int i = 1; i < M; i++){
-        
-        if(i % K != 0){
+    for(let i = 0; i < M; i++){
+        if(i % (K+1) != 0){
             sum += arr_top;
         }
         else{
