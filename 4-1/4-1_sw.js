ddf43d7a260ba717cef08fa35b084be268b6861f
neul0129 <neul0129@naver.com>
2021-08-11 16:38:28 +0900

    final

--- 4-1/4-1_sw.js
+++ 4-1/4-1_sw.js
@@ -1,25 +1,43 @@
-let x = 0;  //현재 위치
-let y = 0;  //현재 위치
+let x = 1;  //현재 위치
+let y = 1;  //현재 위치
 let L = () => {x -= 1};
 let R = () => {x += 1};
 let U = () => {y -= 1};
 let D = () => {y += 1};
 
-let command = [R,R,U] //이동시키기 
+let prev_x = 0;
+let prev_y = 0;
+
+let command = [R,R,R,U,D,D] //이동시키기 
 
 for (const i of command) {  
+    prev_x = x;
+    prev_y = y;
     i();     
-}
-
-if(x <= 5 && y <= 5){
-        if(x < 0){
-                console.log("왼쪽 이동 불가.");
-                x = 0;
+    
+        if(x <= 0){
+             x = prev_x;
+             y = prev_y;
+            continue;
         }
-        if(y < 0){
-                console.log("위쪽 이동 불가");            
-                y = 0;
+        if(y <= 0){
+            x = prev_x;
+            y = prev_y;
+            continue;
         }
-}
+        if(x > 5){
+            console.log("오른쪽 이동 불가");            
+            x = prev_x;
+            y = prev_y;
+            continue;   
+        }
+        if(y > 5){
+            console.log("아래쪽 이동 불가");            
+            x = prev_x;
+            y = prev_y;
+            continue;   
+        }
+        
+    }
 
 console.log(x,y);
