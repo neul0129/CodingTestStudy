7f39080d510b5d3b75424e920a59703b93d19daf
neul0129 <neul0129@naver.com>
2021-08-11 15:46:48 +0900

    together

---
+++ 4-1/4-1_sw.js
@@ -0,0 +1,20 @@
+let x = 0;
+let y = 0;
+let L = () => {x -= 1};
+let R = () => {x += 1};
+let U = () => {y -= 1};
+let D = () => {y += 1};
+let command = [R,R,D]
+while(x <= 5 && y <= 5){
+        if(x < 0){
+                console.log("왼쪽 이동 불가.");
+                break;
+        }
+        if(y < 0){
+                console.log("위쪽 이동 불가");            
+                break;
+        }
+    }
+    R();
+
+console.log(x,y);
