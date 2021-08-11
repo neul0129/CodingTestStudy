85af3dbdb921d554d7711c844b5b881f067182db
neul0129 <neul0129@naver.com>
2021-08-11 16:08:13 +0900

    seungwon

--- 4-1/4-1_sw.js
+++ 4-1/4-1_sw.js
@@ -1,18 +1,24 @@
-let x = 0;
-let y = 0;
+let x = 0;  //현재 위치
+let y = 0;  //현재 위치
 let L = () => {x -= 1};
 let R = () => {x += 1};
 let U = () => {y -= 1};
 let D = () => {y += 1};
-let command = [R,R,D]
-while(x <= 5 && y <= 5){
+
+let command = [R,R,U] //이동시키기 
+
+for (const i of command) {  
+    i();     
+}
+
+if(x <= 5 && y <= 5){
         if(x < 0){
                 console.log("왼쪽 이동 불가.");
-                break;
+                x = 0;
         }
         if(y < 0){
                 console.log("위쪽 이동 불가");            
-                break;
+                y = 0;
         }
 }
 
